45b5e92eff674c5bbade19381cf42b72267394fc
Pratyus Basuli <93593351+BPratyus@users.noreply.github.com>
2023-08-03 22:53:20 +0530

    Add files via upload

---
+++ src/data.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Anand
+ */
+  
+ 
+    
+ public class data{
+    static String Sno,name,last_name,mob_no,mail,city,dist,pwd,gender;
+    
+    void vault(String a,String b,String c,String d,String e,String f,String g){
+        
+        Sno=a;
+        name=b;
+        mob_no=c;
+        mail=d;
+        city=e;
+        pwd=f;
+        gender=g;
+        
+    }
+}
+
+
+
+
+
+    
+
